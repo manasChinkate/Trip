3219841529ced8f853133f6208ac348dd82d7324
Manas <mchinkate1414@gmail.com>
2026-08-16 16:22:06 +0530

    feat(auth): implement user registration with validation and password hashing
    - Added a new migration file for database initialization.
    - Removed obsolete prisma.config.ts file.
    - Updated schema.prisma to include new user fields: password, dob, profilePic, and age.
    - Created skills-lock.json for managing Prisma skills.
    - Refactored auth.controller.ts to implement user registration logic with validation and password hashing.
    - Removed outdated auth.controller.js and auth.routes.js files.

--- server/src/features/auth/auth.controller.ts
+++ server/src/features/auth/auth.controller.ts
@@ -1,4 +1,32 @@
+import { prisma } from "../../../lib/prisma";
 
-export const register = (req: Request, res: Response) => {
-    
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password, role } = req.body;
+  try {
+    UserSchema.parse(req.body);
+  } catch (error) {
+    res.status(400).json({ message: "Schema validation error", error });
+    return;
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const isUserExist = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (isUserExist) {
+      return res
+        .status(409)
+        .json({ status: 409, message: "Email already exist" });
+    }
+    const user = { name, email, password: hashedPassword, role };
+    const createdUser = await prisma.user.create({
+      data: user,
+    });
+    return res.status(201).json({ status: 201, data: createdUser });
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
 };
--- server/src/features/auth/auth.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { register } from "./auth.controller";
-const router = Router();
-router.post("/register", register);
-export default router;
